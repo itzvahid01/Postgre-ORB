4e71fc075cdff55126bf34b62fef2560092eece5
itzvahid01 <amirparsa7131@gmail.com>
2025-05-20 09:15:13 -0700

    gitignore fixed

--- db.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-:: مسیر نسبی به پوشه PostgreSQL Portable
-set PG_PATH=.\.env\pgsql\bin
-set PG_DATA=.\.env\pgsql\data
-
-:: راه‌اندازی PostgreSQL
-echo Starting PostgreSQL...
-%PG_PATH%\pg_ctl -D %PG_DATA% start
